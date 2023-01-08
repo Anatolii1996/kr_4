04c89f34e367767303da39eac747f50573f6f461
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-01-08 16:59:20 +0200

    установил и подключил bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.scss';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { useSelector } from 'react-redux';
 
 import Articles from './components/articles';
+import Cards from './components/cards';
 
 function App() {
   const store = useSelector(store => store);
-  
-  
+
+
   return (
     <div className="App">
-      
-      <Articles/>
+      <Articles />
+      <Cards/>
     </div>
   );
 }
