9924f93aad5feb3749eaf1fcf867d1f1f528ecdc
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-01-14 14:47:51 +0200

    добавил заголовок и кнопки

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,16 @@ import Header from "../components/header";
 const Home = () => {
   return (
     <>
+      <div className="home-header">
+        <h1 className="home-title">Article list</h1>
+        <div>
+          <button className="btn btn-info">Make big cards</button>
+          <button className="btn btn-info">Add articles</button>
+        </div>
+      </div>
       <Cards />
       <Articles />
+      <button className="btn btn-info btn_footer">Show more</button>
     </>
   );
 };
